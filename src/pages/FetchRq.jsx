e58fddd28a4fd8cd826bfc0eccb0985af2bcf5cc
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-09-30 17:45:40 +0530

    Fetch the data using TenstackQuery

--- src/pages/FetchRq.jsx
+++ src/pages/FetchRq.jsx
@@ -1,8 +1,46 @@
+import { getQuotes } from '../API/api'; 
+import { useQuery } from "@tanstack/react-query";
 
 const FetchRq = () => {
-  return (
-    <div>FetchRq</div>
-  )
-}
 
-export default FetchRq
+  const getQuotesData = async () => {
+    try {
+      const res = await getQuotes();
+      console.log(res.data);
+      return res.data; 
+    } catch (error) {
+      console.error('Error fetching quotes:', error);
+      throw new Error('Failed to fetch quotes'); 
+    }
+  };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['quotes'],    // <- it's like useState
+    queryFn: getQuotesData,   // <- it's Like a UseEffect
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>; 
+  }
+
+  if (isError) {
+    return <p>Error fetching data.</p>; 
+  }
+
+  return (
+    <div>
+      {data && data.length > 0 ? (
+        data.map((quote) => (
+          <div key={quote.id} style={{ marginBottom: '20px', border: '1px solid #ccc', padding: '10px' }}>
+            <p>{quote.body}</p>   
+            <p><strong>User ID:</strong> {quote.userId}</p>
+          </div>
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
+    </div>
+  );
+};
+
+export default FetchRq;
