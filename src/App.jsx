51f7edea50548b466446cddd04a4fddd26039034
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-09-30 17:55:31 +0530

    use TenstackQueryDev tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./components/Layouts/MainLayout";
 import Home from "./pages/Home";
 import FetchRq from "./pages/FetchRq";
 import Fetchold from "./pages/Fetchold";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<MainLayout />,
-    children:[
+    path: "/",
+    element: <MainLayout />,
+    children: [
       {
-        path:"/",
-        element:<Home/>    
+        path: "/",
+        element: <Home />,
       },
       {
-        path:"/trad",
-        element:<Fetchold/>
+        path: "/trad",
+        element: <Fetchold />,
       },
       {
-        path:"/rq",
-        element:<FetchRq/>,
-      }
-    ]
-  }
-
-])
+        path: "/rq",
+        element: <FetchRq />,
+      },
+    ],
+  },
+]);
 function App() {
-   const queryClient = new QueryClient();
-
+  const queryClient = new QueryClient();
 
   return (
-  <QueryClientProvider client={queryClient}>
-  <RouterProvider router={router}></RouterProvider>
-  </QueryClientProvider>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
