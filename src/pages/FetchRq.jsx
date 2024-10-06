6bad2d8df4fa81e92e0b5bc80509a5f87d3026d9
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-10-06 09:07:09 +0530

    add refetch related background refetching

--- src/pages/FetchRq.jsx
+++ src/pages/FetchRq.jsx
@@ -1,41 +1,51 @@
-import { getQuotes } from '../API/api'; 
+import { getQuotes } from "../API/api";
 import { useQuery } from "@tanstack/react-query";
 
 const FetchRq = () => {
-
   const getQuotesData = async () => {
     try {
       const res = await getQuotes();
       console.log(res.data);
-      return res.data; 
+      return res.data;
     } catch (error) {
-      console.error('Error fetching quotes:', error);
-      throw new Error('Failed to fetch quotes'); 
+      console.error("Error fetching quotes:", error);
+      throw new Error("Failed to fetch quotes");
     }
   };
 
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['quotes'],    // <- it's like useState
-    queryFn: getQuotesData,   // <- it's Like a UseEffect
-     // gcTime:2000,          // <- Garbage Collection Time
-     staleTime:7000,          // <- Background refetching for new data instead of cached data
+    queryKey: ["quotes"], // <- it's like useState
+    queryFn: getQuotesData, // <- it's Like a UseEffect
+    // gcTime:2000,          // <- Garbage Collection Time
+    // staleTime:7000,          // <- Background refetching for new data instead of cached data
+    refetchInterval: 1000, // <- After 1 sec reftech for fresh data use in stocke related api
+    refetchIntervalInBackground:true //<- it stop when you go athor tab if true then aslo background reftech 
   });
 
   if (isLoading) {
-    return <p>Loading...</p>; 
+    return <p>Loading...</p>;
   }
 
   if (isError) {
-    return <p>Error fetching data.</p>; 
+    return <p>Error fetching data.</p>;
   }
 
   return (
     <div>
       {data && data.length > 0 ? (
         data.map((quote) => (
-          <div key={quote.id} style={{ marginBottom: '20px', border: '1px solid #ccc', padding: '10px' }}>
-            <p>{quote.body}</p>   
-            <p><strong>User ID:</strong> {quote.userId}</p>
+          <div
+            key={quote.id}
+            style={{
+              marginBottom: "20px",
+              border: "1px solid #ccc",
+              padding: "10px",
+            }}
+          >
+            <p>{quote.body}</p>
+            <p>
+              <strong>User ID:</strong> {quote.userId}
+            </p>
           </div>
         ))
       ) : (
